8cf747394146f51d7a787bfe517e1098fc55c0f8
Palanka <david@DESKTOP-VA4N9H5>
2021-03-18 16:58:05 +0100

    ACCEPTAT

---
+++ Acepta_el_Reto/src/acepta_el_reto/Calendari.java
@@ -0,0 +1,46 @@
+package acepta_el_reto;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor..
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Palanka
+ */
+public class Calendari {
+
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        int cas, encont = 0;
+        short[] any;
+
+        cas = ent.nextInt();
+        if (cas > 0) {
+
+            do {
+
+                encont = ent.nextInt();
+                
+                any = new short[encont];
+
+                for (int i = 0; i < any.length; i++) {
+                    any[i] = ent.nextShort();
+                }
+
+                Arrays.sort(any);
+
+
+                System.out.println(any[any.length - 1] - any[0] - encont + 1);
+
+                
+            } while (--cas>0);
+        }
+    }
+
+}
---
+++ Acepta_el_Reto/src/acepta_el_reto/Lamapara.java
@@ -0,0 +1,65 @@
+package acepta_el_reto;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumne
+ */
+public class Lamapara {
+
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        long intentos = 0, horas = 0, encend = 0, miuso = 0, cont = 0;
+
+        intentos = ent.nextInt();
+
+        do {
+            
+            if (cont >= intentos) {
+                break;
+            } else {
+                
+                do {
+                    horas = ent.nextLong();
+                    
+                    if (horas <= 1000000000 && horas > 0) {
+                        break;
+                    }
+                    
+                } while (true);
+
+                do {
+                    encend = ent.nextLong();
+                    if (encend <= 100000000 && encend > 0) {
+                        break;
+                    }
+                } while (true);
+
+                do {
+                    miuso = ent.nextLong();
+                    if (miuso > 0 && miuso <= 10) {
+                        break;
+                    }
+                } while (true);
+
+                if ((miuso * encend >= horas) && (miuso * encend <= horas)) {
+                    System.out.println("AMBAS");
+                } else {
+
+                    if (miuso * encend >= horas) {
+                        System.out.println("HORAS");
+                    }else{
+                    
+                        System.out.println("ENCENDIDOS");
+                    }
+                }
+
+                
+            }
+            cont++;
+        } while (true);
+
+    }
+}
