b2d7db9656d6caf73caaf67ca64b3ba08e3d29be
Palanka <david@DESKTOP-VA4N9H5>
2021-03-18 16:47:33 +0100

    ACCEPTAT

--- Acepta-el-Reto/src/acepta_el_reto/Calendari.java
+++ Acepta-el-Reto/src/acepta_el_reto/Calendari.java
@@ -3,7 +3,7 @@ package acepta_el_reto;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor..
  */
 
 import java.util.Arrays;
